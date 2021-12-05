2de17945333a68ef6832e4fc21ff0c26e59b69df
Amex090 <64318732+Amex090@users.noreply.github.com>
2021-12-05 14:35:19 +0300

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -3,4 +3,4 @@ from transformers import pipeline
 classifier = pipeline("sentiment-analysis",   
                       "SkolkovoInstitute/russian_toxicity_classifier")
 
-classifier("Я обожаю инженерию машинного обучения!")
+classifier("Люблю макдоналдс!")
