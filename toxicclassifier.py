e5f66d51467ff1b97297552b7f3165f7af07f04e
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-01-04 23:26:28 +0300

    Update toxicclassifier.py

--- toxicclassifier.py
+++ toxicclassifier.py
@@ -9,9 +9,7 @@ app = FastAPI()
 classifier = pipeline("sentiment-analysis",   
                       "distilbert-base-uncased-finetuned-sst-2-english")
 
-@app.get("/root/")
-def root():
-    return {"message": "Hello World"}
+
 
 @app.post("/predict/")
 def predict(item: Item):
