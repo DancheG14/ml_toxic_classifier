63a82dc051e6cb99e23e966664b95e2ac6cf76cb
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-01-04 21:20:12 +0300

    Delete classifier.py

--- classifier.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis",   
-                      "SkolkovoInstitute/russian_toxicity_classifier")
-
-classifier("Люблю макдоналдс!")
