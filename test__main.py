5b52ba4bec646a9ee32c2a039978a13118b19e0a
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-04-10 13:40:17 +0300

    Update test__main.py

--- test__main.py
+++ test__main.py
@@ -9,12 +9,12 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-   
+
 def test_read_predict_positive():
     response = client.post("/predict/",
                            json={"text": "I like machine learning!"}
-                          )
-    json_data = response.json() 
+                           )
+    json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
@@ -22,7 +22,7 @@ def test_read_predict_positive():
 def test_read_predict_negative():
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"}
-                          )
+                           )
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
