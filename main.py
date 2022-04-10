2b46153ea749bbac35ccf9daf25a45e51e531e90
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-04-10 13:13:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ class Item(BaseModel):
 
         
 app = FastAPI()
-classifier = pipeline("sentiment-analysis",   
-                      "distilbert-base-uncased-finetuned-sst-2-english")
+classifier = pipeline("sentiment-analysis", "distilbert-base-uncased-finetuned-sst-2-english")
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message":"Hello World"}
 
 
 @app.post("/predict/")
-def predict(item: Item):
+def predict(item:Item):
     return classifier(item.text)[0]
