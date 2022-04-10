772d822fcaf91abd2414187c8fd986060a47d658
Amex090 <64318732+Amex090@users.noreply.github.com>
2022-04-10 13:10:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     text: str
 
+        
 app = FastAPI()
 classifier = pipeline("sentiment-analysis",   
                       "distilbert-base-uncased-finetuned-sst-2-english")
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
+
 @app.post("/predict/")
 def predict(item: Item):
-    return classifier(item.text )[0]
+    return classifier(item.text)[0]
